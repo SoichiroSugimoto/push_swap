f4afe0b9c23165a3ea500e74d321d52b3faf16e7
SoichiroSugimoto <fcsoichi1998@gmail.com>
2021-07-24 22:28:45 +0900

    add checker.c

---
+++ include/checker.h
@@ -0,0 +1,15 @@
+#ifndef CHECKER_H
+# define CHECKER_H
+
+# include "insert_contents.h"
+# include "use_operations.h"
+# include "sort_number.h"
+# include "utils.h"
+# include "../libft/libft.h"
+# include <string.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <unistd.h>
+
+
+#endif
