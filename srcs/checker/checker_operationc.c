74d6287554d46c4df0fb7f2f94e08b750f3080be
SoichiroSugimoto <fcsoichi1998@gmail.com>
2021-12-07 22:21:05 +0900

    fix checker's segf

--- srcs/checker/checker_operationc.c
+++ srcs/checker/checker_operationc.c
@@ -6,7 +6,7 @@
 /*   By: sosugimo <sosugimo@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 19:49:57 by sosugimo          #+#    #+#             */
-/*   Updated: 2021/11/29 19:49:58 by sosugimo         ###   ########.fr       */
+/*   Updated: 2021/12/07 22:09:48 by sosugimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,27 @@
 
 void	ss_checker(t_list **lst_a, t_list **lst_b)
 {
-	swap_operation(lst_a);
-	swap_operation(lst_b);
+	if (*lst_a && *lst_b)
+	{
+		swap_operation(lst_a);
+		swap_operation(lst_b);
+	}
 }
 
 void	rr_checker(t_list **lst_a, t_list **lst_b)
 {
-	rotate_operation(lst_a);
-	rotate_operation(lst_b);
+	if (*lst_a && *lst_b)
+	{
+		rotate_operation(lst_a);
+		rotate_operation(lst_b);
+	}
 }
 
 void	rrr_checker(t_list **lst_a, t_list **lst_b)
 {
-	reverse_rotate_operation(lst_a);
-	reverse_rotate_operation(lst_b);
+	if (*lst_a && *lst_b)
+	{
+		reverse_rotate_operation(lst_a);
+		reverse_rotate_operation(lst_b);
+	}
 }
